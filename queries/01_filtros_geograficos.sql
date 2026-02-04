7c200a9ab952e6b4a81a0083a20af791c39a9d7c
GabrielFarizel <gabriel.farizell@gmail.com>
2026-02-03 23:08:19 -0300

    Create 01_filtros_geograficos.sql

---
+++ queries/01_filtros_geograficos.sql
@@ -0,0 +1,12 @@
+-- Análise geográfica
+-- Objetivo: Contar cidades distintas por estado (SP e RJ)
+
+SELECT
+    g.geolocation_state,
+    COUNT(DISTINCT g.geolocation_city) AS cidades
+FROM geolocation g
+WHERE
+    g.geolocation_state IN ('SP', 'RJ')
+    AND g.geolocation_lat > -24.54
+    AND g.geolocation_lng < -45.63
+GROUP BY g.geolocation_state;
